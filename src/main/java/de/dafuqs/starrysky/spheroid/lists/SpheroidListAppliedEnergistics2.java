790d132938514f732ef39a49608fae292f16a530
DaFuqs <dominik.m.schmidt@googlemail.com>
2021-01-09 12:48:52 +0100

    Removed cloth api dependency

--- src/main/java/de/dafuqs/starrysky/spheroid/lists/SpheroidListAppliedEnergistics2.java
+++ src/main/java/de/dafuqs/starrysky/spheroid/lists/SpheroidListAppliedEnergistics2.java
@@ -10,6 +10,7 @@ import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
 import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.block.BlockState;
+import net.minecraft.item.Item;
 import net.minecraft.loot.UniformLootTableRange;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.util.Identifier;
@@ -35,13 +36,13 @@ public class SpheroidListAppliedEnergistics2 extends SpheroidList {
         Identifier  appliedenergistics_sky_stone_chest_loot_table = new Identifier(StarrySkyCommon.MOD_ID, "appliedenergistics2_meteor_chest");
 
         CoreSpheroidType       APPLIEDENERGISTRICS_QUARTZ         = new CoreSpheroidType(null, 5, 7, appliedenergistics_quartz, MAP_STONES, 5, 8);
-        DoubleCoreSpheroidType APPLIEDENERGISTRICS_CHARGED_QUARTZ = new DoubleCoreSpheroidType(null, 5, 8, appliedenergistics_quartz, appliedenergistics_charged_quartz, MAP_STONES, 1, 2, 3, 5);
+        DoubleCoreSpheroidType APPLIEDENERGISTRICS_CHARGED_QUARTZ = new DoubleCoreSpheroidType(null, 5, 8, appliedenergistics_charged_quartz, appliedenergistics_quartz, MAP_STONES, 1, 2, 3, 5);
         ModularSpheroidType    APPLIEDENERGISTRICS_ASTEROID       = new ModularSpheroidType(null, 8, 14, appliedenergistics_sky_stone)
                 .addCenterChestWithLoot(appliedenergistics_sky_stone_chest_loot_table);
 
-        spheroidLoader.registerSpheroidType(OVERWORLD, SpheroidDistributionType.ORE, 0.8F, APPLIEDENERGISTRICS_QUARTZ);
-        spheroidLoader.registerSpheroidType(OVERWORLD, SpheroidDistributionType.ORE, 0.3F, APPLIEDENERGISTRICS_CHARGED_QUARTZ);
-        spheroidLoader.registerSpheroidType(OVERWORLD, SpheroidDistributionType.ESSENTIAL, 0.5F, APPLIEDENERGISTRICS_ASTEROID);
+        spheroidLoader.registerSpheroidType(OVERWORLD, SpheroidDistributionType.ORE, 1.0F, APPLIEDENERGISTRICS_QUARTZ);
+        spheroidLoader.registerSpheroidType(OVERWORLD, SpheroidDistributionType.ORE, 0.4F, APPLIEDENERGISTRICS_CHARGED_QUARTZ);
+        spheroidLoader.registerSpheroidType(OVERWORLD, SpheroidDistributionType.ESSENTIAL, 0.9F, APPLIEDENERGISTRICS_ASTEROID);
 
         // Only fill AE2 loot table with items when AE2 is loaded.
         // Otherwise the items don't even exist and vanilla throws
@@ -49,17 +50,17 @@ public class SpheroidListAppliedEnergistics2 extends SpheroidList {
         Identifier APPLIED_ENERGISTICS_METEOR_CHEST_LOOT_TABLE = new Identifier("starry_sky", "appliedenergistics2_meteor_chest");
         LootTableLoadingCallback.EVENT.register((resourceManager, lootManager, id, supplier, setter) -> {
             if (APPLIED_ENERGISTICS_METEOR_CHEST_LOOT_TABLE.equals(id)) {
-                Identifier CALCULATION_PRESS = new Identifier("appliedenergistics2", "calculation_processor_press");
-                Identifier ENGINEERING_PRESS = new Identifier("appliedenergistics2", "engineering_processor_press");
-                Identifier LOGIC_PRESS = new Identifier("appliedenergistics2", "logic_processor_press");
-                Identifier SILICON_PRESS = new Identifier("appliedenergistics2", "silicon_press");
+                Item CALCULATION_PRESS = Registry.ITEM.get(new Identifier("appliedenergistics2", "calculation_processor_press"));
+                Item ENGINEERING_PRESS = Registry.ITEM.get(new Identifier("appliedenergistics2", "engineering_processor_press"));
+                Item LOGIC_PRESS = Registry.ITEM.get(new Identifier("appliedenergistics2", "logic_processor_press"));
+                Item SILICON_PRESS = Registry.ITEM.get(new Identifier("appliedenergistics2", "silicon_press"));
 
                 FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
                         .rolls(UniformLootTableRange.between(1, 3))
-                        .withEntry(ItemEntry.builder(Registry.ITEM.get(CALCULATION_PRESS)).build())
-                        .withEntry(ItemEntry.builder(Registry.ITEM.get(ENGINEERING_PRESS)).build())
-                        .withEntry(ItemEntry.builder(Registry.ITEM.get(LOGIC_PRESS)).build())
-                        .withEntry(ItemEntry.builder(Registry.ITEM.get(SILICON_PRESS)).build());
+                        .withEntry(ItemEntry.builder(CALCULATION_PRESS).build())
+                        .withEntry(ItemEntry.builder(ENGINEERING_PRESS).build())
+                        .withEntry(ItemEntry.builder(LOGIC_PRESS).build())
+                        .withEntry(ItemEntry.builder(SILICON_PRESS).build());
                 supplier.withPool(poolBuilder.build());
             }
         });
