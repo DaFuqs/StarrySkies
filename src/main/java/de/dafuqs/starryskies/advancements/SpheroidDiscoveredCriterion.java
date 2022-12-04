717fe099d5446b34529cf365bb8ce9e5c3c9d724
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-12-04 21:09:58 +0100

    advancements finished

--- src/main/java/de/dafuqs/starryskies/advancements/SpheroidDiscoveredCriterion.java
+++ src/main/java/de/dafuqs/starryskies/advancements/SpheroidDiscoveredCriterion.java
@@ -1,7 +1,8 @@
 package de.dafuqs.starryskies.advancements;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
 import de.dafuqs.starryskies.StarrySkies;
 import de.dafuqs.starryskies.spheroids.spheroids.Spheroid;
 import net.minecraft.advancement.criterion.AbstractCriterion;
@@ -11,7 +12,6 @@ import net.minecraft.predicate.entity.AdvancementEntityPredicateSerializer;
 import net.minecraft.predicate.entity.EntityPredicate;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.JsonHelper;
 import org.jetbrains.annotations.Nullable;
 
 public class SpheroidDiscoveredCriterion extends AbstractCriterion<SpheroidDiscoveredCriterion.Conditions> {
@@ -23,8 +23,30 @@ public class SpheroidDiscoveredCriterion extends AbstractCriterion<SpheroidDisco
 	}
 	
 	public SpheroidDiscoveredCriterion.Conditions conditionsFromJson(JsonObject jsonObject, EntityPredicate.Extended extended, AdvancementEntityPredicateDeserializer advancementEntityPredicateDeserializer) {
-		Identifier spheroidIdentifier = Identifier.tryParse(JsonHelper.getString(jsonObject, "id"));
-		return new SpheroidDiscoveredCriterion.Conditions(extended, spheroidIdentifier);
+		Identifier[] identifiers;
+		if(jsonObject.has("ids")) {
+			identifiers = deserializeAll(jsonObject.get("ids"));
+		} else {
+			identifiers = new Identifier[0];
+		}
+		return new SpheroidDiscoveredCriterion.Conditions(extended, identifiers);
+	}
+	
+	private static Identifier[] deserializeAll(JsonElement json) {
+		JsonArray array = json.getAsJsonArray();
+		Identifier[] ids = new Identifier[array.size()];
+		for(int i = 0; i < array.size(); i++) {
+			ids[i] = Identifier.tryParse(array.get(i).getAsString());
+		}
+		return ids;
+	}
+	
+	private static JsonElement serializeAll(Identifier[] identifiers) {
+		JsonArray array = new JsonArray();
+		for(Identifier id : identifiers) {
+			array.add(id.toString());
+		}
+		return array;
 	}
 	
 	public void trigger(ServerPlayerEntity player, Spheroid spheroid) {
@@ -33,21 +55,32 @@ public class SpheroidDiscoveredCriterion extends AbstractCriterion<SpheroidDisco
 	
 	public static class Conditions extends AbstractCriterionConditions {
 		
-		private final Identifier spheroidIdentifier;
+		private final Identifier[] identifiers;
 		
-		public Conditions(EntityPredicate.Extended player, @Nullable Identifier spheroidIdentifier) {
+		public Conditions(EntityPredicate.Extended player, @Nullable Identifier[] identifiers) {
 			super(ID, player);
-			this.spheroidIdentifier = spheroidIdentifier;
+			this.identifiers = identifiers;
 		}
 		
 		public JsonObject toJson(AdvancementEntityPredicateSerializer predicateSerializer) {
 			JsonObject jsonObject = super.toJson(predicateSerializer);
-			jsonObject.add("id", new JsonPrimitive(String.valueOf(this.spheroidIdentifier)));
+			jsonObject.add("ids", serializeAll(identifiers));
 			return jsonObject;
 		}
 		
 		public boolean matches(Identifier spheroidIdentifier) {
-			return spheroidIdentifier != null && (this.spheroidIdentifier == null || this.spheroidIdentifier.equals(spheroidIdentifier));
+			if(this.identifiers.length == 0) {
+				return true;
+			}
+			if(spheroidIdentifier == null) {
+				return true;
+			}
+			for(Identifier id : identifiers) {
+				if(spheroidIdentifier.equals(id)) {
+					return true;
+				}
+			}
+			return false;
 		}
 	}
 	
