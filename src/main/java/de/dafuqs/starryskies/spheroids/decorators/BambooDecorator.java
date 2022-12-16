a28d7796562e2b1c83a8615683c576a82cd4b180
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-12-16 15:51:11 +0100

    made bamboo decoratior modular

--- src/main/java/de/dafuqs/starryskies/spheroids/decorators/BambooDecorator.java
+++ src/main/java/de/dafuqs/starryskies/spheroids/decorators/BambooDecorator.java
@@ -5,46 +5,52 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import de.dafuqs.starryskies.spheroids.SpheroidDecorator;
 import de.dafuqs.starryskies.spheroids.spheroids.Spheroid;
 import net.minecraft.block.BambooBlock;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.enums.BambooLeaves;
+import net.minecraft.command.argument.BlockArgumentParser;
+import net.minecraft.util.JsonHelper;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.random.Random;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.StructureWorldAccess;
 
 public class BambooDecorator extends SpheroidDecorator {
 	
-	private static final Block bambooBlock = Blocks.BAMBOO;
-	private static final int BAMBOO_CHANCE = 13;
-	private static final BlockState bambooBlockState = Blocks.BAMBOO.getDefaultState().with(BambooBlock.AGE, 0).with(BambooBlock.STAGE, 0);
-	private static final BlockState bambooSaplingBlockState = Blocks.BAMBOO_SAPLING.getDefaultState();
+	private final float chance;
+	private final float saplingChance;
+	private final BlockState bambooBlockState;
+	private final BlockState bambooSaplingBlockState;
 	
 	public BambooDecorator(JsonObject data) throws CommandSyntaxException {
 		super(data);
+		this.chance = JsonHelper.getFloat(data, "chance");
+		this.saplingChance = JsonHelper.getFloat(data, "sapling_chance");
+		this.bambooBlockState = BlockArgumentParser.block(Registry.BLOCK, JsonHelper.getString(data, "bamboo_block"), false).blockState();
+		this.bambooSaplingBlockState = BlockArgumentParser.block(Registry.BLOCK, JsonHelper.getString(data, "sapling_block"), false).blockState();
 	}
 	
 	@Override
 	public void decorate(StructureWorldAccess world, ChunkPos origin, Spheroid spheroid, Random random) {
 		for (BlockPos bp : getTopBlocks(world, origin, spheroid)) {
-			int r = random.nextInt(BAMBOO_CHANCE);
-			
-			if (r == 0) {
-				if (bambooBlock.canPlaceAt(bambooBlockState, world, bp.up())) {
-					world.setBlockState(bp.up(), bambooSaplingBlockState, 3);
-				}
-			} else if (r < 8) {
-				for (int i = 1; i < r; i++) {
-					if (bambooBlock.canPlaceAt(bambooBlockState, world, bp.up(i))) {
-						if (i == 3 && r < 5) {
-							world.setBlockState(bp.up(i), bambooBlockState.with(BambooBlock.LEAVES, BambooLeaves.NONE), 3);
-						} else if (i > 4) {
-							world.setBlockState(bp.up(i), bambooBlockState.with(BambooBlock.LEAVES, BambooLeaves.LARGE), 3);
-						} else if (i > 2) {
-							world.setBlockState(bp.up(i), bambooBlockState.with(BambooBlock.LEAVES, BambooLeaves.SMALL), 3);
-						} else {
-							world.setBlockState(bp.up(i), bambooBlockState.with(BambooBlock.LEAVES, BambooLeaves.NONE), 3);
+			if (random.nextFloat() < chance) {
+				if (random.nextFloat() < saplingChance) {
+					if (bambooSaplingBlockState.canPlaceAt(world, bp.up())) {
+						world.setBlockState(bp.up(), bambooSaplingBlockState, 3);
+					}
+				} else {
+					int height = random.nextInt(8);
+					for (int i = 1; i < height; i++) {
+						if (bambooBlockState.canPlaceAt(world, bp.up(i))) {
+							if (i == 3 && height < 5) {
+								world.setBlockState(bp.up(i), bambooBlockState.with(BambooBlock.LEAVES, BambooLeaves.NONE), 3);
+							} else if (i > 4) {
+								world.setBlockState(bp.up(i), bambooBlockState.with(BambooBlock.LEAVES, BambooLeaves.LARGE), 3);
+							} else if (i > 2) {
+								world.setBlockState(bp.up(i), bambooBlockState.with(BambooBlock.LEAVES, BambooLeaves.SMALL), 3);
+							} else {
+								world.setBlockState(bp.up(i), bambooBlockState.with(BambooBlock.LEAVES, BambooLeaves.NONE), 3);
+							}
 						}
 					}
 				}
