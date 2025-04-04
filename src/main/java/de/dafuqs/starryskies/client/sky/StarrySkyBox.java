5a0df343cb76b74cfc43542968b1bc4298e51fb6
DaFuqs <dominik.m.schmidt@googlemail.com>
2025-04-04 22:13:48 +0200

    tickdelta gaming

--- src/main/java/de/dafuqs/starryskies/client/sky/StarrySkyBox.java
+++ src/main/java/de/dafuqs/starryskies/client/sky/StarrySkyBox.java
@@ -5,13 +5,14 @@ import de.dafuqs.starryskies.*;
 import net.fabricmc.api.*;
 import net.fabricmc.fabric.api.client.rendering.v1.*;
 import net.minecraft.block.enums.*;
+import net.minecraft.client.*;
 import net.minecraft.client.render.*;
 import net.minecraft.entity.*;
 import net.minecraft.entity.effect.*;
 import net.minecraft.util.*;
+import net.minecraft.util.math.*;
 import org.joml.*;
 
-// TODO: Rainbow Skybox setting does not work
 @Environment(EnvType.CLIENT)
 public class StarrySkyBox implements DimensionRenderingRegistry.SkyRenderer {
 
@@ -56,18 +57,18 @@ public class StarrySkyBox implements DimensionRenderingRegistry.SkyRenderer {
 		RenderSystem.depthMask(false);
 		RenderSystem.setShader(GameRenderer::getPositionTexColorProgram);
 		Tessellator tessellator = Tessellator.getInstance();
-		BufferBuilder bufferBuilder = tessellator.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE_COLOR);
 		
 		Matrix4f matrix4f = context.positionMatrix();
 		
-		/*Vec3d vec3d = context.world().getSkyColor(context.camera().getPos(), 0);  // TODO: how to get tickdelta?
+		float tickDelta = MinecraftClient.getInstance().getRenderTickCounter().getTickDelta(false);
+		Vec3d vec3d = context.world().getSkyColor(context.camera().getPos(), tickDelta);
 		float f = (float)vec3d.x;
 		float g = (float)vec3d.y;
 		float h = (float)vec3d.z;
-		RenderSystem.setShaderColor(f, g, h, 1.0F);*/
+		RenderSystem.setShaderColor(f, g, h, 1.0F);
 		
 		RenderSystem.setShaderTexture(0, DOWN);
-		bufferBuilder = tessellator.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE_COLOR);
+		BufferBuilder bufferBuilder = tessellator.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE_COLOR);
 		bufferBuilder.vertex(matrix4f, -100.0f, -100.0f, -100.0f).texture(0.0F, 0.0F).color(255, 255, 255, 255);
 		bufferBuilder.vertex(matrix4f, -100.0f, -100.0f, 100.0f).texture(0.0F, 1.0F).color(255, 255, 255, 255);
 		bufferBuilder.vertex(matrix4f, 100.0f, -100.0f, 100.0f).texture(1.0F, 1.0F).color(255, 255, 255, 255);
